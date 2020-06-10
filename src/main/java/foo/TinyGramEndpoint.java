6ea549c71385d04059adc58f47c19f0ec3dd63a8
SHombert <sarah.hombert@gmail.com>
2020-06-10 11:54:35 +0200

    brouillon endpoint

---
+++ src/main/java/foo/TinyGramEndpoint.java
@@ -0,0 +1,154 @@
+package foo;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+
+import com.google.api.server.spi.auth.common.User;
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiMethod.HttpMethod;
+import com.google.api.server.spi.config.ApiNamespace;
+import com.google.api.server.spi.config.Named;
+import com.google.api.server.spi.config.Nullable;
+import com.google.api.server.spi.response.CollectionResponse;
+import com.google.api.server.spi.response.UnauthorizedException;
+import com.google.api.server.spi.auth.EspAuthenticator;
+
+import com.google.appengine.api.datastore.Cursor;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.PropertyProjection;
+import com.google.appengine.api.datastore.PreparedQuery.TooManyResultsException;
+import com.google.appengine.api.datastore.Query.CompositeFilter;
+import com.google.appengine.api.datastore.Query.CompositeFilterOperator;
+import com.google.appengine.api.datastore.Query.Filter;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.appengine.api.datastore.QueryResultList;
+import com.google.appengine.api.datastore.Transaction;
+
+@Api(name = "myApi",
+     version = "v1",
+     audiences = "852096843957-2vb1jhobb8t1311kqf2v269chbbmp2km.apps.googleusercontent.com",
+  	 clientIds = "852096843957-2vb1jhobb8t1311kqf2v269chbbmp2km.apps.googleusercontent.com",
+     namespace =
+     @ApiNamespace(
+		   ownerDomain = "tinygram-272116.appspot.com/",
+		   ownerName = "tinygram-272116.appspot.com/",
+		   packagePath = "")
+     )
+class TinyGramEndpoint {
+
+	/*@ApiMethod(name = "mypost", httpMethod = HttpMethod.GET)
+	public CollectionResponse<Entity> mypost(@Named("name") String name, @Nullable @Named("next") String cursorString) {
+
+	    Query q = new Query("Post").setFilter(new FilterPredicate("owner", FilterOperator.EQUAL, name));
+
+	    // https://cloud.google.com/appengine/docs/standard/python/datastore/projectionqueries#Indexes_for_projections
+	    //q.addProjection(new PropertyProjection("body", String.class));
+	    //q.addProjection(new PropertyProjection("date", java.util.Date.class));
+	    //q.addProjection(new PropertyProjection("likec", Integer.class));
+	    //q.addProjection(new PropertyProjection("url", String.class));
+
+	    // looks like a good idea but...
+	    // generate a DataStoreNeedIndexException -> 
+	    // require compositeIndex on owner + date
+	    // Explosion combinatoire.
+	    // q.addSort("date", SortDirection.DESCENDING);
+	    
+	    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+	    PreparedQuery pq = datastore.prepare(q);
+	    
+	    FetchOptions fetchOptions = FetchOptions.Builder.withLimit(2);
+	    
+	    if (cursorString != null) {
+		fetchOptions.startCursor(Cursor.fromWebSafeString(cursorString));
+		}
+	    
+	    QueryResultList<Entity> results = pq.asQueryResultList(fetchOptions);
+	    cursorString = results.getCursor().toWebSafeString();
+	    
+	    return CollectionResponse.<Entity>builder().setItems(results).setNextPageToken(cursorString).build();
+	    
+	}
+    
+	@ApiMethod(name = "getPost",
+		   httpMethod = ApiMethod.HttpMethod.GET)
+	public CollectionResponse<Entity> getPost(User user, @Nullable @Named("next") String cursorString)
+			throws UnauthorizedException {
+
+		if (user == null) {
+			throw new UnauthorizedException("Invalid credentials");
+		}
+
+		Query q = new Query("Post").
+		    setFilter(new FilterPredicate("owner", FilterOperator.EQUAL, user.getEmail()));
+
+		// Multiple projection require a composite index
+		// owner is automatically projected...
+		// q.addProjection(new PropertyProjection("body", String.class));
+		// q.addProjection(new PropertyProjection("date", java.util.Date.class));
+		// q.addProjection(new PropertyProjection("likec", Integer.class));
+		// q.addProjection(new PropertyProjection("url", String.class));
+
+		// looks like a good idea but...
+		// require a composite index
+		// - kind: Post
+		//  properties:
+		//  - name: owner
+		//  - name: date
+		//    direction: desc
+
+		// q.addSort("date", SortDirection.DESCENDING);
+
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		PreparedQuery pq = datastore.prepare(q);
+
+		FetchOptions fetchOptions = FetchOptions.Builder.withLimit(2);
+
+		if (cursorString != null) {
+			fetchOptions.startCursor(Cursor.fromWebSafeString(cursorString));
+		}
+
+		QueryResultList<Entity> results = pq.asQueryResultList(fetchOptions);
+		cursorString = results.getCursor().toWebSafeString();
+
+		return CollectionResponse.<Entity>builder().setItems(results).setNextPageToken(cursorString).build();
+	}
+	
+	@ApiMethod(name = "postMsg", httpMethod = HttpMethod.POST)
+	public Entity postMsg(User user, PostMessage pm) throws UnauthorizedException {
+
+		if (user == null) { // valider authentification
+			throw new UnauthorizedException("Invalid credentials");
+		}
+
+		Entity e = new Entity("Post", Long.MAX_VALUE-(new Date()).getTime()+":"+user.getEmail());
+		e.setProperty("owner", user.getEmail());
+		e.setProperty("url", pm.url);
+		e.setProperty("body", pm.body);
+		e.setProperty("likec", 0);
+		e.setProperty("date", new Date());
+
+///		Solution pour pas projeter les listes
+//		Entity pi = new Entity("PostIndex", e.getKey());
+//		HashSet<String> rec=new HashSet<String>();
+//		pi.setProperty("receivers",rec);
+		
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		Transaction txn = datastore.beginTransaction();
+		datastore.put(e);
+//		datastore.put(pi);
+		txn.commit();
+		return e;
+	}*/
+}
